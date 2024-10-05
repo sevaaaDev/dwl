faa89b4891b8037659f4353b022d0b08ebc00f21
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-10-05 17:59:23 +0700

    conf: set keybind for resizing floating window

--- config.def.h
+++ config.def.h
@@ -187,5 +187,5 @@ static const Key keys[] = {
 static const Button buttons[] = {
 	{ MODKEY, BTN_LEFT,   moveresize,     {.ui = CurMove} },
 	{ MODKEY, BTN_MIDDLE, togglefloating, {0} },
-	{ MODKEY, BTN_RIGHT,  moveresize,     {.ui = CurResize} },
+	{ MODKEY|WLR_MODIFIER_SHIFT, BTN_LEFT,  moveresize,     {.ui = CurResize} },
 };
